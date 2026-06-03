34765c6d214c096bb15ff0309f617d9103eb58c5
Kamta Rawlings <kamtarawlings@gmail.com>
2026-06-03 20:24:26 +0100

    Added the test for deterministic hashing

--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -8,3 +8,7 @@ def test_k_positions():
 def test_in_range():
     for p in get_positions("hello", 10, 500):
         assert 0 <= p < 500
+
+
+def test_deterministic():
+    assert get_positions("abc", 5, 100) == get_positions("abc", 5, 100)
