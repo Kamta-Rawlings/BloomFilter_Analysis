23276cf8257bb5f679d51b3d9e1d53b9c7a7c599
Kamta Rawlings <kamtarawlings@gmail.com>
2026-06-03 19:00:04 +0100

    generat the Bloom filter bit positions from hash values

--- bloomfilter/hashing.py
+++ bloomfilter/hashing.py
@@ -15,3 +15,12 @@ def _two_hashes(item):
     d1 = hashlib.blake2b(data, digest_size=8, person=b"bloom_h1").digest()
     d2 = hashlib.blake2b(data, digest_size=8, person=b"bloom_h2").digest()
     return int.from_bytes(d1, "big"), int.from_bytes(d2, "big")
+
+
+def get_positions(item, k, m):
+    """Return k bit positions in [0, m) for the given item."""
+    h1, h2 = _two_hashes(item)
+    out = []
+    for i in range(k):
+        out.append((h1 + i * h2) % m)
+    return out
