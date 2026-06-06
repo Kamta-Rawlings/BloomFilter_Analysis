6bdf7c215e64af55826099d2404d228b26195e95
Ihame-b <ihamegrbt@gmail.com>
2026-06-06 20:22:47 +0200

    Implement memory usage comparison between set and Bloom filter

--- benchmarks/experiment_compression.py
+++ benchmarks/experiment_compression.py
@@ -6,3 +6,28 @@ SIZES = [1000, 10000, 100000, 500000]
 os.makedirs("results", exist_ok=True)
 
 rows = []
+
+for n in SIZES:
+    s = set()
+    for i in range(n):
+        s.add("item-" + str(i))
+
+    set_bytes = sys.getsizeof(s) + sum(sys.getsizeof(x) for x in s)
+
+    bf = BloomFilter(n, 0.01)
+    for i in range(n):
+        bf.add("item-" + str(i))
+
+    bloom_bytes = bf.m // 8
+
+    rows.append({
+        "n": n,
+        "set_bytes": set_bytes,
+        "bloom_bytes": bloom_bytes,
+        "compression_ratio": set_bytes / bloom_bytes,
+    })
+
+    print(
+        "n=%d  set=%d B  bloom=%d B  ratio=%.1fx"
+        % (n, set_bytes, bloom_bytes, set_bytes / bloom_bytes)
+    )
