f4cbbe27bd279f0ae7be645006befe1b1ce9724b
Ihame-b <ihamegrbt@gmail.com>
2026-06-06 13:24:21 +0200

    Implement add and lookup performance benchmarks

--- benchmarks/benchmark_ops.py
+++ benchmarks/benchmark_ops.py
@@ -4,3 +4,28 @@ from bloomfilter import BloomFilter
 SIZES = [1000, 10000, 100000, 500000]
 
 os.makedirs("results", exist_ok=True)
+
+rows = []
+
+for n in SIZES:
+    bf = BloomFilter(n, 0.01)
+
+    t0 = time.perf_counter()
+    for i in range(n):
+        bf.add("item-" + str(i))
+    t_add = time.perf_counter() - t0
+
+    t0 = time.perf_counter()
+    for i in range(n):
+        _ = "item-" + str(i) in bf
+    t_look = time.perf_counter() - t0
+
+    rows.append({
+        "n": n,
+        "add_total_s": t_add,
+        "lookup_total_s": t_look,
+        "add_us_per_op": t_add / n * 1e6,
+        "lookup_us_per_op": t_look / n * 1e6,
+    })
+
+    print("n=%d  add=%.3fs  lookup=%.3fs" % (n, t_add, t_look))
