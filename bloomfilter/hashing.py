5a4ef778598ee80d19bfd643e66b334aa4603319
Kamta Rawlings <kamtarawlings@gmail.com>
2026-06-11 11:52:53 +0100

    implement Bloom filter index generation using double hashing

--- bloomfilter/hashing.py
+++ bloomfilter/hashing.py
@@ -47,3 +47,35 @@ def blake2_h1(item):
 def blake2_h2(item):
     d = hashlib.blake2b(_to_bytes(item), digest_size=8, person=b"bloom_h2").digest()
     return int.from_bytes(d, "big")
+
+
+HASHES = {
+    "blake2_h1": blake2_h1,
+    "blake2_h2": blake2_h2,
+    "djb2": djb2,
+    "sdbm": sdbm,
+    "fnv1a": fnv1a,
+}
+
+
+# --- default positions: Kirsch-Mitzenmacher double hashing with blake2b ---
+
+def get_positions(item, k, m):
+    """Return k bit positions in [0, m) using double hashing."""
+    h1 = blake2_h1(item)
+    h2 = blake2_h2(item)
+    out = []
+    for i in range(k):
+        out.append((h1 + i * h2) % m)
+    return out
+
+
+def get_positions_pair(item, k, m, hf1, hf2):
+    """Same as get_positions but lets you pick which two hashes to combine.
+    Used by the hash-comparison experiment."""
+    a = hf1(item)
+    b = hf2(item)
+    out = []
+    for i in range(k):
+        out.append((a + i * b) % m)
+    return out
